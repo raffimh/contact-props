ca2f069de2d24df98e747d7f9745a730a5b2effa
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2020-12-25 20:01:14 +0700

    initial commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Eren Jaeger",
+    imgURL:
+      "https://static.wikia.nocookie.net/shingekinokyojin/images/a/a1/Eren_Jaeger_%28Anime%29_character_image.png",
+    phone: "+123 456 789",
+    email: "eren@paradis.com",
+  },
+  {
+    name: "Reiner Braun",
+    imgURL:
+      "https://static.wikia.nocookie.net/shingekinokyojin/images/6/6d/Reiner_Braun_%28Anime%29_character_image.png",
+    phone: "+987 654 321",
+    email: "reiner@marley.com",
+  },
+  {
+    name: "Annie Leonhart",
+    imgURL:
+      "https://static.wikia.nocookie.net/shingekinokyojin/images/6/66/Annie_Leonhart_%28Anime%29_character_image.png",
+    phone: "+918 372 574",
+    email: "annie@marley.com",
+  },
+];
+
+export default contacts;
